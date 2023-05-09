f4c46598c1a076b2a161c85ec8b96409c4d9504a
Eric Ma <ericma2024@u.northwestern.edu>
2023-05-09 18:57:20 -0500

    merge;

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Dashboard from './components/Dashboard'
-import NavBar from './components/NavBar'
+import Dashboard from "./components/Dashboard";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import { Link, BrowserRouter, Route, Routes } from "react-router-dom";
 
-
+import { useDbData } from "./utilities/firebase";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, error] = useDbData("/");
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Dashboard />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Home data={data} />
+              </div>
+            }
+          />
+
+          <Route
+            path="/:eventId"
+            element={
+              <div>
+                <Dashboard />
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import NavBar from "./NavBar";
+import Event from "./Event";
+import "../styles/Home.css";
+
+const Dashboard = ({ data }) => {
+  if (!data) {
+    return <h1>Data is loading...</h1>;
+  }
+  console.log(data);
+  return (
+    <div>
+      <NavBar />
+      <h1 id="header"> Events </h1>
+      <div className="feed">
+        {data.Events.map((event, index) => (
+          <Event data={event} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
