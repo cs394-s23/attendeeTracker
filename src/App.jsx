cebbe96486f3e9b6bce9464a6dceba9b64290777
Eric Ma <ericma2024@u.northwestern.edu>
2023-05-09 17:22:29 -0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Dashboard from './components/Dashboard'
-import NavBar from './components/NavBar'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Dashboard from "./components/Dashboard";
+import NavBar from "./components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <Dashboard />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
