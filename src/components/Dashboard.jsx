eaa479fd26317fc64f61d056eafea911a788d503
Yiran Mo <yiranmo2023@gmail.com>
2023-05-09 19:28:08 -0500

    Fix Dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -19,23 +19,6 @@ const Dashboard = () => {
         <button>Send Reminder</button>
       </div>
       <DashChartCard />
-const Dashboard = () => {
-  return (
-    <div>
-      <NavBar />
-      <div className="dashboard">
-        <h1>Data Science Event</h1>
-        <DashInfoCard className="info-card" />
-        <DashAttendanceCard className="attendance-card" />
-        <div className="analytics">
-          <h2>Response Rate:</h2>
-
-          <h2>Graph:</h2>
-        </div>
-        <div className="reminder">
-          <button>Send Reminder</button>
-        </div>
-      </div>
     </div>
   );
 };
