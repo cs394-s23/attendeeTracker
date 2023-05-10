d008e7b3fdfea4699187e44fbf1c711793f44e2a
Eric Ma <ericma2024@u.northwestern.edu>
2023-05-09 19:15:30 -0500

    fixing css

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,29 +1,26 @@
-import "../styles/Dashboard.css"
-import NavBar from "./NavBar"
-import DashInfoCard from "./dashInfoCard"
-import DashAttendanceCard from "./dashAttendanceCard"
+import "../styles/Dashboard.css";
+import NavBar from "./NavBar";
+import DashInfoCard from "./dashInfoCard";
+import DashAttendanceCard from "./dashAttendanceCard";
 const Dashboard = () => {
-    return (
-        <div className="dashboard">
-            <NavBar />
-            <h1>Data Science Event</h1>
-            <DashInfoCard/>
-            <DashAttendanceCard/>
-            <div className="analytics">
-                <h2>Response Rate:</h2>
+  return (
+    <div>
+      <NavBar />
+      <div className="dashboard">
+        <h1>Data Science Event</h1>
+        <DashInfoCard className="info-card" />
+        <DashAttendanceCard className="attendance-card" />
+        <div className="analytics">
+          <h2>Response Rate:</h2>
 
-                <h2>Graph:</h2>
-
-            </div>
-            <div className="reminder">
-                <button>Send Reminder</button>
-            </div>
-
-
-
+          <h2>Graph:</h2>
         </div>
-    )
-}
+        <div className="reminder">
+          <button>Send Reminder</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-
-export default Dashboard
+export default Dashboard;
