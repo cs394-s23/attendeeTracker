f6517c2ae18b494753516448ed7a5428b37a80f6
Eric Ma <ericma2024@u.northwestern.edu>
2023-05-09 19:30:00 -0500

    fix navbar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,20 +5,22 @@ import DashAttendanceCard from "./dashAttendanceCard";
 import DashChartCard from "./DashChartCard";
 const Dashboard = () => {
   return (
-    <div className="dashboard">
+    <div>
       <NavBar />
-      <h1>Data Science Event</h1>
-      <DashInfoCard />
-      <DashAttendanceCard />
-      <div className="analytics">
-        <h2>Response Rate:</h2>
+      <div className="dashboard">
+        <h1>Data Science Event</h1>
+        <DashInfoCard />
+        <DashAttendanceCard />
+        <div className="analytics">
+          <h2>Response Rate:</h2>
 
-        <h2>Graph:</h2>
+          <h2>Graph:</h2>
+        </div>
+        <div className="reminder">
+          <button>Send Reminder</button>
+        </div>
+        <DashChartCard />
       </div>
-      <div className="reminder">
-        <button>Send Reminder</button>
-      </div>
-      <DashChartCard />
     </div>
   );
 };
